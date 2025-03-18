a61818ed61367d2813e2ec2e6cfd9fbe00662a89
Daniel Portugal <dpcosta@gmail.com>
2025-03-18 05:27:22 -0300

    Mudanças do vídeo.

--- src/Contextos/ContainRs.Vendas/Propostas/AprovarProposta.cs
+++ src/Contextos/ContainRs.Vendas/Propostas/AprovarProposta.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ContainRs.Vendas.Locacoes;
 using System.Transactions;
+using ContainRs.Contracts;
 
 namespace ContainRs.Vendas.Propostas;
 
@@ -18,31 +19,4 @@ public class AprovarProposta
 
     public Guid IdPedido { get; }
     public Guid IdProposta { get; }
-
-    public void Executar()
-    {
-        var proposta = await repoProposta
-                .GetFirstAsync(
-                    p => p.Id == propostaId && p.SolicitacaoId == id,
-                    p => p.Id);
-        if (proposta is null) return Results.NotFound();
-
-        proposta.Situacao = SituacaoProposta.Aceita;
-
-        // criar locação a partir da proposta aceita
-        var locacao = new Locacao()
-        {
-            PropostaId = proposta.Id,
-            DataInicio = DateTime.Now,
-            DataPrevistaEntrega = proposta.Solicitacao.DataInicioOperacao.AddDays(-proposta.Solicitacao.DisponibilidadePrevia),
-            DataTermino = proposta.Solicitacao.DataInicioOperacao.AddDays(proposta.Solicitacao.DuracaoPrevistaLocacao)
-        };
-
-        using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
-
-        await repoProposta.UpdateAsync(proposta);
-        await repoLocacao.AddAsync(locacao);
-
-        scope.Complete();
-    }
 }
--- src/Contextos/ContainRs.Vendas/Propostas/PropostasEndpoints.cs
+++ src/Contextos/ContainRs.Vendas/Propostas/PropostasEndpoints.cs
@@ -121,14 +121,11 @@ public static class PropostasEndpoints
         builder.MapPatch("{id:guid}/proposals/{propostaId:guid}/accept", async (
             [FromRoute] Guid id,
             [FromRoute] Guid propostaId,
-            [FromServices] IRepository<Proposta> repoProposta,
-            [FromServices] IRepository<Locacao> repoLocacao) =>
+            [FromServices] IPropostaService service) =>
         {
             var casoUso = new AprovarProposta(id, propostaId);
-            casoUso.Executar
-
-            
-
+            var proposta = await service.AprovarAsync(casoUso);            
+            if (proposta is null) return Results.NotFound();
             return Results.Ok(PropostaResponse.From(proposta));
         })
         .WithSummary("Cliente aceita proposta de locação.")
