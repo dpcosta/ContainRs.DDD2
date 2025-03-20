0e9a413ecd69016bab89269f81f6fdd459121b12
Daniel Portugal <dpcosta@gmail.com>
2025-03-20 12:13:57 -0300

    Mudanças do ensaio, parte 1

--- src/Contextos/ContainRs.Financeiro/Faturamento/EmissorFaturaJob.cs
+++ src/Contextos/ContainRs.Financeiro/Faturamento/EmissorFaturaJob.cs
@@ -1,26 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using ContainRs.Contracts;
 using Microsoft.Extensions.Logging;
 
 namespace ContainRs.Financeiro.Faturamento;
 
+public class PropostaAprovadaDto
+{
+    public Guid IdProposta { get; set; }
+    public decimal ValorProposta { get; set; }
+}
+
 public class EmissorFaturaJob
 {
-    private readonly ILogger<EmissorFaturaJob> _logger;
     public const string INBOX_ID = "emissor-fatura";
+    private readonly ILogger<EmissorFaturaJob> _logger;
+    private readonly IRepository<Fatura> repoFatura;
+    private readonly IEventManager eventManager;
 
-    public EmissorFaturaJob(ILogger<EmissorFaturaJob> logger)
+    public EmissorFaturaJob(ILogger<EmissorFaturaJob> logger, IRepository<Fatura> repoFatura, IEventManager eventManager)
     {
         _logger = logger;
+        this.repoFatura = repoFatura;
+        this.eventManager = eventManager;
     }
 
-    public Task ExecutarAsync()
+    public async Task ExecutarAsync()
     {
         _logger.LogInformation("Executando o emissor de fatura...");
-        
-        return Task.CompletedTask;
+
+        var mensagens = await eventManager
+            .GetNotProcessedAsync<PropostaAprovadaDto>("PropostaAprovada", nameof(EmissorFaturaJob));
+
+        foreach (var proposta in mensagens)
+        {
+            // gerar fatura a partir da proposta
+            var fatura = new Fatura()
+            {
+                Id = Guid.NewGuid(),
+                Numero = $"{DateTime.Now.Year}000000001",
+                DataEmissao = DateTime.Now,
+                DataVencimento = DateTime.Now.AddDays(5),
+                //LocacaoId = ? recuperar via ACL
+                Total = proposta.ValorProposta,
+            };
+            await repoFatura.AddAsync(fatura);
+        }
     }
 }
