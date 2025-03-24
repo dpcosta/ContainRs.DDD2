0ec1de92c23ef3588addebd9a8e41e11e931a99d
Daniel Portugal <dpcosta@gmail.com>
2025-03-24 05:05:49 -0300

    Mudanças do vídeo

---
+++ src/Contextos/ContainRs.Financeiro/Faturamento/EmissorDeFaturas.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContainRs.Contracts;
+
+namespace ContainRs.Financeiro.Faturamento;
+
+public class PropostaAprovada
+{
+    public Guid IdProposta { get; set; }
+    public decimal ValorProposta { get; set; }
+}
+
+public class EmissorDeFaturas
+{
+    private readonly IRepository<Fatura> repoFatura;
+
+    public EmissorDeFaturas(IRepository<Fatura> repoFatura)
+    {
+        this.repoFatura = repoFatura;
+    }
+
+    public async Task ExecutarAsync()
+    {
+        var mensagens = new List<PropostaAprovada>();
+
+        foreach(var mensagem in mensagens)
+        {
+            Fatura fatura = new()
+            {
+                Id = Guid.NewGuid(),
+                DataEmissao = DateTime.Now,
+                DataVencimento = DateTime.Now.AddDays(5),
+                Numero = "304823908",
+                Total = mensagem.ValorProposta,
+                //LocacaoId = ?? // ACL
+            };
+
+            // persistir a fatura
+            await repoFatura.AddAsync(fatura);
+
+            // marcar msg como lida
+        }
+    }
+}
